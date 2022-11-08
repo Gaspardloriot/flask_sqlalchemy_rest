b9c37a5b4223b4454647452c53695325149ce2b9
Gaspard Loriot-Boserup <gaspardloriot@gmail.com>
2022-11-08 16:01:21 +0000

    make it better

--- src/routers/client_files.py
+++ src/routers/client_files.py
@@ -1,7 +1,19 @@
+import os
 from fastapi import  File, UploadFile
 from config.app import app
+import pandas as pd
 
 
 @app.post("/uploadfile")
 async def create_upload_file(file: UploadFile= File(...)):
+    contents = await file.read()   
+    output = open('test.xls', 'wb')
+    output.write(contents)
+    output.close()
+    df = pd.read_excel('./test.xls')
+    print(df.head())
+    os.remove('test.xls')
+
+
+
     return {"filename": file.filename, "test" : "hello from python"}
